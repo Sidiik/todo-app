8d7d548da08f160d21efd300fb5a54ce92bc4ff1
Sadiq O <sadiqomar890@gmail.com>
2021-10-06 19:27:43 +0300

    init

---
+++ src/components/Form.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { FormGroup, Form, FormLabel } from "react-bootstrap";
+
+const Form = () => {
+  return (
+    <div className="container">
+      <Form>
+        <FormGroup>
+          <FormLabel>Write a todo</FormLabel>
+          <F
+        </FormGroup>
+      </Form>
+    </div>
+  );
+};
+
+export default Form;
